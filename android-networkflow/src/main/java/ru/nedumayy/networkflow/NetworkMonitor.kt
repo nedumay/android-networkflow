499733ff2d8aceff1cdca8559a60ae680aa1f325
Samim <samim058@gmail.com>
2025-12-16 18:13:24 +0300

    Обновление NetworkMonitor

--- android-networkflow/src/main/java/ru/nedumayy/networkflow/NetworkMonitor.kt
+++ android-networkflow/src/main/java/ru/nedumayy/networkflow/NetworkMonitor.kt
@@ -3,6 +3,9 @@ package ru.nedumayy.networkflow
 import kotlinx.coroutines.flow.StateFlow
 import ru.nedumayy.networkflow.state.NetworkState
 
+/**
+ * Интерфейс для реализация состояния сети
+ */
 interface NetworkMonitor {
     val networkState: StateFlow<NetworkState>
 }
